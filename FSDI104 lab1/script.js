f8aedf052ce0f66fbb812b5619f32284f21227f6
michael-zirkle <48534665+michael-zirkle@users.noreply.github.com>
2019-04-16 16:24:19 -0400

    Lab 1

---
+++ FSDI104 lab1/script.js
@@ -0,0 +1,9 @@
+//array
+
+const students=["Mike"," Michelle"," James"," John"," Vickie"," Joe"," Bill"," Bob"," Nick"," Leon"];
+
+const grades= [60,65,70,75,80,58,85,90,95,88];
+
+document.write(students+" "+grades);
+
+
