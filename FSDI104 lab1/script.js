1b6d55b5fceb4c5024873a0f72ab51c9beba8911
michael-zirkle <48534665+michael-zirkle@users.noreply.github.com>
2019-04-18 20:11:56 -0400

    Update script.js

--- FSDI104 lab1/script.js
+++ FSDI104 lab1/script.js
@@ -1,9 +1,16 @@
-//array
+
 
 const students=["Mike"," Michelle"," James"," John"," Vickie"," Joe"," Bill"," Bob"," Nick"," Leon"];
 
 const grades= [60,65,70,75,80,58,85,90,95,88];
 
+var total = 0;
+for(var i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+var avg = total / grades.length;
+
+
+
 document.write(students+" "+grades);
-
-
+window.alert(avg)
